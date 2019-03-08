0ef1ea4ae1af6d4e4659f29013ac6d73b6b67cd9
karavias <dimitris@rokoko.co>
2019-03-08 17:20:41 +0100

    Command API integration. The following requests can be sent to Studio: Calibrate: Will calibrate all Smartsuit Pro. Unicast: Will make live connected Smartsuit Pro unicast. Broadcast: Will make live connected Smartsuit Pro broadcast. Start Recording: Will trigger studio to start a recording. Stop Recording: Will trigger studio to stop recording.

---
+++ Assets/Rokoko/CommandAPI/RequestData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RequestData
+{
+    public string smartsuit = "";
+    public float countdown_delay = 4;
+    public string filename = "";
+
+    public override string ToString()
+    {
+        return smartsuit + "," + countdown_delay + ", " + filename;
+    }
+
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
